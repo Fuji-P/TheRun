26432cffc54daf4c2d789e490f65303ed576fefb
Fuji-P <nyaguri0417@gmail.com>
2022-05-31 21:14:28 +0900

    初回コミット

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // 定数定義
+    private const int MAX_SCORE = 999999;       // スコア最大数
+    public int stageNo;                         // ステージナンバー
+    public GameObject textGameOver;             // 「ゲームオーバー」テキスト
+    public GameObject textClear;                // 「クリア」テキスト
+    public GameObject buttons;                  // 操作ボタン
+    public GameObject textScoreNumber;          // スコアテキスト
+    public enum GAME_MODE                       // ゲーム状態定義
+    {
+        PLAY,                                   // プレイ中
+        CLEAR,                                  // クリア
+        GAMEOVER,                               // ゲームオーバー
+    };
+    public GAME_MODE gameMode = GAME_MODE.PLAY; // ゲーム状態
+
+    private int score = 0;                      // スコア
+    private int displayScore = 0;               // 表示用スコア
+
+    // Sounds
+    public AudioClip clearSE;                   // 効果音:クリア
+    public AudioClip gameoverSE;                // 効果音:ゲームオーバー
+    private AudioSource audioSource;            // オーディオソース
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
+        RefreshScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score > displayScore)
+        {
+            displayScore += 10;
+            if (displayScore > score) displayScore = score;
+            RefreshScore();
+		}
+    }
+
+    // ゲームオーバー処理
+    public void GameOver()
+	{
+        audioSource.PlayOneShot(gameoverSE);
+        textGameOver.SetActive(true);
+        buttons.SetActive(false);
+        Invoke("GoBackStageSelect", 2.0f);
+    }
+
+    // ゲームクリア処理
+    public void GameClear()
+    {
+        audioSource.PlayOneShot(clearSE);
+        gameMode = GAME_MODE.CLEAR;
+        textClear.SetActive(true);
+        buttons.SetActive(false);
+        
+        // セーブデータ更新
+        if (PlayerPrefs.GetInt("CLEAR", 0) < stageNo)
+        {
+            PlayerPrefs.GetInt("CLEAR", stageNo);
+            Invoke("GoBackStageSelect", 2.0f);
+        }
+    }
+
+    // スコア加算
+    public void AddScore(int val)
+    {
+        score += val;
+        if (score > MAX_SCORE) score = MAX_SCORE;
+	}
+
+    // スコア表示を更新
+    void RefreshScore()
+    {
+        textScoreNumber.GetComponent<Text>().text = displayScore.ToString();
+	}
+
+    // ステージセレクトシーンに戻る
+    void GoBackStageSelect()
+    {
+        SceneManager.LoadScene("StageSelectScene");
+	}
+}
---
+++ Assets/Scripts/OrbManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class OrbManager : MonoBehaviour
+{
+    private const int ORB_POINT = 100;      // オーブの特典
+    private GameObject gameManager;         // ゲームマネージャー
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // オーブ入手処理
+    public void GetOrb()
+    {
+        gameManager.GetComponent<GameManager>().AddScore(ORB_POINT);
+
+        // コライダーを削除
+        CircleCollider2D circleCollider = GetComponent<CircleCollider2D>();
+        Destroy(circleCollider);
+
+        // 消失アニメーション
+        transform.DOScale(2.5f, 0.3f);
+        SpriteRenderer spriteRenderer = transform.GetComponent<SpriteRenderer>();
+        DOTween.ToAlpha(() => spriteRenderer.color, a => spriteRenderer.color = a, 0.0f, 0.3f);
+
+        Destroy(this.gameObject, 0.5f);
+	}
+}
